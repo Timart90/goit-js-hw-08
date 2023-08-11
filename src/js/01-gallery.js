003311c1d41680198a6fa7d0b0eadd39e87c85ca
Timart90 <118985642+Timart90@users.noreply.github.com>
2023-08-11 17:20:26 +0300

    fix Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,46 +5,30 @@ import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 
 
-console.log(galleryItems);
+// console.log(galleryItems);
 
 
-function createrImg (arr) {
-  return arr.map(({ preview, original, description }) =>
+function galleryImg(arr) {
+   return arr.map(({ preview, original, description }) =>
   `<li class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
 </li>`).join('')
+
 };
 
 const ul = document.querySelector('.gallery');
-
 ul.addEventListener('click', handlerClick);
-ul.insertAdjacentHTML('beforeend', createrImg(galleryItems));
-
+ul.insertAdjacentHTML('beforeend', galleryImg(galleryItems));
 
 function handlerClick(e) {
   e.preventDefault();
-  if (e.target.nodeName !== 'IMG') {
-    return;
-  
-}
-
-const fullImg = e.target.dataset.source;
-const instance = SimpleLightbox.create(`
-    <img src="${fullImg}" width="800" height="600">`)
-  instance.show();
-  
-  ul.addEventListener('keydown', (e) => {
-  if (e.code === 'Escape') {
-    instance.close();
-  }
-});
 };
+  
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+})
 
 
