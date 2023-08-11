01368a3967b1ad843ee925750b577dbdd10ff3ec
Timart90 <118985642+Timart90@users.noreply.github.com>
2023-08-11 16:50:34 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,50 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 
 console.log(galleryItems);
+
+
+function createrImg (arr) {
+  return arr.map(({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join('')
+};
+
+const ul = document.querySelector('.gallery');
+
+ul.addEventListener('click', handlerClick);
+ul.insertAdjacentHTML('beforeend', createrImg(galleryItems));
+
+
+function handlerClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  
+}
+
+const fullImg = e.target.dataset.source;
+const instance = SimpleLightbox.create(`
+    <img src="${fullImg}" width="800" height="600">`)
+  instance.show();
+  
+  ul.addEventListener('keydown', (e) => {
+  if (e.code === 'Escape') {
+    instance.close();
+  }
+});
+};
+
+
